0063526443db5e1ad35bfb1fca9b5699222b4fb4
tatmanblue <Matt.Raffel@gmail.com>
2020-01-16 13:26:56 -0700

    a smidgen of refactor

---
+++ Game/Assets/My Game/Code/Inventory/InventoryObjectTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CornTheory.Inventory
+{
+    public enum InventoryObjectTypes
+    {
+        NotConfigured,  // This is an error and should never be a value seen at runtime
+        General,        // Sellable but no other value
+    }
+}
