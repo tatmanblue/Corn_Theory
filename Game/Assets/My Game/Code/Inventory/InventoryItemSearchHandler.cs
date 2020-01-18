99cb98166157e662f72c175e538890a46293e9da
tatmanblue <Matt.Raffel@gmail.com>
2020-01-18 14:42:11 -0700

    gets the basics of holding down the mouse for a given time to trigger a give
    Signed-off-by: tatmanblue <Matt.Raffel@gmail.com>

--- Game/Assets/My Game/Code/Inventory/InventoryItemSearchHandler.cs
+++ Game/Assets/My Game/Code/Inventory/InventoryItemSearchHandler.cs
@@ -1,11 +1,10 @@
 ﻿using UnityEngine;
-using UnityEngine.EventSystems;
 using CornTheory.Scriptables;
 
 
 namespace CornTheory.Inventory
 {
-    public class InventoryItemSearchHandler : MonoBehaviour, IPointerClickHandler
+    public class InventoryItemSearchHandler : MonoBehaviour
     {
         [SerializeField] InventoryDescriptionObject Item;
         [SerializeField] int GiveQuanity;
@@ -26,6 +25,22 @@ namespace CornTheory.Inventory
             player.AddInventoryItem(Item, GiveQuanity);
         }
 
+        /// <summary>
+        /// checks to see if the mouse position is over the object this script is attached to
+        /// </summary>
+        /// <returns></returns>
+        private bool IsMouseOnSelf()
+        {
+            Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+                if (0 == hit.transform.name.CompareTo(transform.name))
+                    return true;
+
+            return false;
+        }
+
         private void Update()
         {
             if (true == HasGivenItem)
@@ -42,18 +57,10 @@ namespace CornTheory.Inventory
 
             if (Input.GetMouseButtonDown(0))
             {
-                Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-
-                if (Physics.Raycast(ray, out hit))
+                if (true == IsMouseOnSelf())
                 {
-                    print(string.Format("HIT DATA: {0}", hit.transform.name));
-                    if (0 == hit.transform.name.CompareTo(transform.name))
-                    {
-                        print(string.Format("InventoryItemSearchHandler mouse down for {0}", transform.name));
-                        startTime = Time.time;
-                        endTime = InventoryItemSearchHandler.NOT_SET;
-                    }
+                    startTime = Time.time;
+                    endTime = InventoryItemSearchHandler.NOT_SET;
                 }
             }
 
@@ -62,9 +69,19 @@ namespace CornTheory.Inventory
                 // only care about this if startTime was set
                 if (InventoryItemSearchHandler.NOT_SET < startTime)
                 {
-                    print(string.Format("InventoryItemSearchHandler mouse up for {0}", transform.name));
-                    endTime = Time.time;
-                    print(string.Format("duration {0}", endTime - startTime));
+                    // the rule is in order for this to count has holding the mouse down
+                    // for the duration it has to be released over this object.  if it is
+                    // released elsewhere, then it doesnt count and the attempt
+                    // has to start over
+                    if (true == IsMouseOnSelf())
+                    {
+                        endTime = Time.time;
+                    }
+                    else
+                    {
+                        startTime = InventoryItemSearchHandler.NOT_SET;
+                        endTime = InventoryItemSearchHandler.NOT_SET;
+                    }
                 }
             }
 
@@ -76,13 +93,9 @@ namespace CornTheory.Inventory
 
                 Player.Player player = GameObject.FindGameObjectWithTag("Player").transform.GetComponent<Player.Player>();
                 GiveItem(player);
-                print("given inventory");
+                print(string.Format("{0} gave inventory item {1}", transform.name, Item.name));
             }
         }
 
-        public void OnPointerClick(PointerEventData eventData)
-        {
-            print(string.Format("I got the click {0}", transform.name));
-        }
     }
 }
